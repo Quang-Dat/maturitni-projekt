89bc749bd23de05a83d84a7120b48612d68358bd
Dat Le Quang <lequangdat06@seznam.cz>
2024-12-20 21:29:34 +0100

    nefunkcni produkce

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,10 +12,11 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Playfair+Display+SC:ital,wght@0,400;0,700;0,900;1,400;1,700;1,900&display=swap" rel="stylesheet">
 
+    @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @vite(['resources/css/welcome.css'])
     @vite(['resources/js/nav.js'])
-
+    @endif
 
 </head>
 
